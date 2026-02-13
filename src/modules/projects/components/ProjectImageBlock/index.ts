e4196d01b1111eeef23b612cb0b63530d707aee7
zhavoronskaya <helenheykey@gmail.com>
2026-02-13 23:21:26 +0100

    fix():ci

--- src/modules/projects/components/ProjectImageBlock/index.ts
+++ src/modules/projects/components/ProjectImageBlock/index.ts
@@ -1,8 +1,8 @@
-export { ProjectImageBlock } from "./ProjectImageBlock";
-export { ProjectGallery } from "./ProjectGallery";
-export type { ProjectGalleryBlock } from "./ProjectGallery";
-export type {
-  ProjectImageBlockLayout,
-  ProjectImageBlockItem,
-  ProjectImageBlockProps,
-} from "./types";
+// export { ProjectImageBlock } from "./ProjectImageBlock";
+// export { ProjectGallery } from "./ProjectGallery";
+// export type { ProjectGalleryBlock } from "./ProjectGallery";
+// export type {
+//   ProjectImageBlockLayout,
+//   ProjectImageBlockItem,
+//   ProjectImageBlockProps,
+// } from "./types";
